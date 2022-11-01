a88d25926e7ffd0bc0f42eae2aa86f6f48d647f6
v.hoangnv46 <v.hoangnv46@vinbigdata.com>
2022-11-01 11:37:42 +0700

    update

---
+++ jenkins-template/job-dsl/bookapp.groovy
@@ -0,0 +1,34 @@
+def bookapp_api_list = [
+    [service_name: "book-api", job_name: "book-api", values_local_parse_pattern: ".image.tag", jenkinsfile_template: "Jenkinsfile.local.common", default_branch: 'master']
+    [service_name: "customer-api", job_name: "customer-api", values_local_parse_pattern: ".image.tag", jenkinsfile_template: "Jenkinsfile.local.common", default_branch: 'master']
+    [service_name: "order-api", job_name: "order-api", values_local_parse_pattern: ".image.tag", jenkinsfile_template: "Jenkinsfile.local.common", default_branch: 'master']
+]
+
+bookapp_api_list.each { api ->
+    pipelineJob() {
+        environmentVariables {
+            env('SERVICE_NAME', "${api['service_name']}")
+            env('VALUES_LOCAL_PARSE_PATTERN', "${api['values_local_parse_pattern']}")
+        }
+        parameters {
+            stringParam('BRANCH', "${api['default_branch']}", 'Description: Specify branch to build')
+        }
+        description()
+        keepDependencies(false)
+        logRotator {
+            artifactNumToKeep(2)
+            numToKeep(5)
+        }
+        properties {
+            disableConcurrentBuilds {
+                abortPrevious(false)
+            }
+        }
+        definition {
+            cps {
+                script(readFileFromWorkspace("templates/${api['jenkinsfile_template']}"))
+                sandbox()
+            }
+        }
+    }
+}
